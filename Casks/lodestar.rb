882228561dbf6b4ae956416b17e476f8b4357716
Rocco Vaccone <rocco@vaccone.us>
2026-08-16 09:48:22 -0400

    Lodestar 0.11.4

--- Casks/lodestar.rb
+++ Casks/lodestar.rb
@@ -1,6 +1,6 @@
 cask "lodestar" do
-  version "0.11.3"
-  sha256 "f80adaca1874eb8cf04fa67882ff8a3c1a84557553b5b47c31254e05d183ad9d"
+  version "0.11.4"
+  sha256 "f8e0afef1b0a991cd7cdfda81ff24bb7e8fcd2c4c9b2880dbecbba1df98935bc"
 
   url "https://github.com/Vaccone-Software/lodestar/releases/download/v#{version}/lodestar-#{version}.zip"
   name "Lodestar"
